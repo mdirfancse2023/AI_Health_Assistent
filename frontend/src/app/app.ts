f68d45b21782b5ee0032bcb2bf90b16afb391e17
Md Irfan <mdirfancse2023@gmail.com>
2026-03-26 22:11:15 +0530

    Navbar added

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `<router-outlet></router-outlet>`
+  imports: [RouterModule, RouterOutlet],
+  templateUrl: './app.html',        // ✅ moved here
+  styleUrls: ['./app.css']          // ✅ css linked here
 })
 export class AppComponent {}
